170f2db0d131ea3d80a4718576b46699daa5f794
udon-universe <128375799+udon-universe@users.noreply.github.com>
2023-03-21 18:04:26 +0900

    add: necessary module

--- scripts/template.py
+++ scripts/template.py
@@ -2,7 +2,7 @@ import modules.scripts as scripts
 import gradio as gr
 import os
 
-from modules import images
+from modules import images, script_callbacks
 from modules.processing import process_images, Processed
 from modules.processing import Processed
 from modules.shared import opts, cmd_opts, state
